cb28754d81ea2a68584f50077c149f6edec2838f
airmanxtw <airmanx@stust.edu.tw>
2025-03-05 23:11:10 +0800

    新增 MutiInput.vue 組件，並在 Home17View.vue 中整合以處理多個輸入值

---
+++ src/components/MutiInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input v-for="(item, index) in props.modelValue" :key="index" :value="item"
+    @input="(e) => updateItem(index, (e.target as HTMLInputElement).value)" />
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string[];
+}>();
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string[]): void;
+}>();
+import { List } from "immutable";
+const updateItem = (index: number, value: string) => {
+  debugger;
+  emit('update:modelValue', List(...props.modelValue).setIn([index], value).toArray());
+}
+
+</script>
--- src/views/Home17View.vue
+++ src/views/Home17View.vue
@@ -2,11 +2,18 @@
   <HComponents4 />
   <input v-for="(item) in data" :key="item.key" v-model="item.value" />
   {{ data }}
+  <hr />
+  <MutiInput v-model="data2" />
+  {{ data2 }}
 </template>
 <script setup lang="ts">
 import HComponents4 from '@/components/HComponents4.vue';
+import MutiInput from '@/components/MutiInput.vue';
 import { ref } from 'vue';
-const data=ref<KeyValue[]>([{key:'one',value:'2'}, {key:'two',value:'4'},{key:'three',value:'6'}]);
+
+const data = ref<KeyValue[]>([{ key: 'one', value: '2' }, { key: 'two', value: '4' }, { key: 'three', value: '6' }]);
+
+const data2 = ref<string[]>(['hello1', 'hell2', 'hello3']);
 
 
 </script>
