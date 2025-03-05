df21b5fee97b1cc5fd79d3aa3a82945ee4d12d5b
airmanxtw <airmanx@stust.edu.tw>
2025-03-06 00:21:19 +0800

    更新 MutiInput.vue 組件以支援嵌套輸入，並在 Home17View.vue 中調整數據結構以符合新功能

--- src/components/MutiInput.vue
+++ src/components/MutiInput.vue
@@ -1,18 +1,35 @@
 <template>
-  <input v-for="(item, index) in props.modelValue" :key="index" :value="item"
-    @input="(e) => updateItem(index, (e.target as HTMLInputElement).value)" />
+  <ol>
+    <li v-for="(item, index) in props.modelValue" :key="index">
+      <input v-if="!Array.isArray(item)" :data-value="[...props.index,index]" :value="item" @input="(e) => updateItem(index, e.target as HTMLInputElement)" />
+      <MutiInput v-else :model-value="item" :index="[...props.index,index]" />
+    </li>
+  </ol>
+
 </template>
 <script setup lang="ts">
+import MutiInput from '@/components/MutiInput.vue';
+
 const props = defineProps<{
-  modelValue: string[];
+  modelValue: (string | string[])[] | string[];
+  index:number[];
 }>();
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: string[]): void;
+  (e: 'update:modelValue', value: (string | string[])[] | string[]): void;
 }>();
 import { List } from "immutable";
-const updateItem = (index: number, value: string) => {
+const updateItem = (index: number, input: HTMLInputElement) => {
+  const test=`[${input.dataset.value}]`;
+  const test2:number[]=JSON.parse(test);
+
+  const data0=List([1,2,3,4,[1,2,3]]);
+  const data1=data0.setIn([4,1],99);
   debugger;
-  emit('update:modelValue', List(...props.modelValue).setIn([index], value).toArray());
+  console.log(data1.toJS());
+  //const data1=List(props.modelValue).setIn(test2, input.value).toJS();
+  // debugger;
+  //console.log(data1.toJS());
+  emit('update:modelValue', List(props.modelValue).setIn(test2, input.value).toJS());
 }
 
 </script>
--- src/views/Home17View.vue
+++ src/views/Home17View.vue
@@ -3,7 +3,7 @@
   <input v-for="(item) in data" :key="item.key" v-model="item.value" />
   {{ data }}
   <hr />
-  <MutiInput v-model="data2" />
+  <MutiInput v-model="data2" :index="[]" />
   {{ data2 }}
 </template>
 <script setup lang="ts">
@@ -13,7 +13,7 @@ import { ref } from 'vue';
 
 const data = ref<KeyValue[]>([{ key: 'one', value: '2' }, { key: 'two', value: '4' }, { key: 'three', value: '6' }]);
 
-const data2 = ref<string[]>(['hello1', 'hell2', 'hello3']);
+const data2 = ref(['hello1', 'hell2', 'hello3',[ 'hello4', 'hello5', 'hello6']]);
 
 
 </script>
