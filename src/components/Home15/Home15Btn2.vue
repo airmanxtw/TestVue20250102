68e4fa6d4b44292fbaf714d19030e9675c37be02
airmanxtw <airmanx@stust.edu.tw>
2025-03-12 00:27:31 +0800

    在 Home15Btn2 和 Home15Btn22 組件中新增 v-model 支援，以便於數據綁定

--- src/components/Home15/Home15Btn2.vue
+++ src/components/Home15/Home15Btn2.vue
@@ -1,6 +1,8 @@
 <template>
-  <Home15Btn22/>
+  <Home15Btn22 v-model="model"/>
+  {{ model }}
 </template>
 <script setup lang="ts">
 import Home15Btn22 from '@/components/Home15/Home15Btn22.vue';
+const model=defineModel();
 </script>
--- src/components/Home15/Home15Btn22.vue
+++ src/components/Home15/Home15Btn22.vue
@@ -1,9 +1,11 @@
 <template>
     <div style="border:1px solid red;padding: 10px;">
       <button v-bind="$attrs">click me</button>
+      <input v-model="model" />
     </div>
 </template>
 <script setup lang="ts">
 defineOptions({inheritAttrs: false});
+const model=defineModel();
 </script>
 
